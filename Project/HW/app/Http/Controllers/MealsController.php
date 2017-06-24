dfc78cb51f2fc43c59c525765a4569f70a199285
Russell Pena <russellpena@Russells-MacBook-Pro.local>
2017-06-24 10:51:22 -0400

    added meals migration

---
+++ Project/HW/app/Http/Controllers/MealsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Meals;
+use Illuminate\Http\Request;
+
+class MealsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Meals  $meals
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Meals $meals)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Meals  $meals
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Meals $meals)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Meals  $meals
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Meals $meals)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Meals  $meals
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Meals $meals)
+    {
+        //
+    }
+}
---
+++ Project/HW/database/migrations/2017_06_24_142632_create_meals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Meal');
+            $table->string('Food');
+            $table->integer('Carbs');
+            $table->integer('Calories');
+            $table->integer('Protein');
+            $table->integer('Fat');
+            $table->timestamp('added_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+}
